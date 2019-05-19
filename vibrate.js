ac4f2b42e8810130d542c4664f94214962eabcdb
Toothbrush <toothbrush.draw@gmail.com>
2019-05-19 13:27:19 -0700

    Event calendar

--- vibrate.js
+++ vibrate.js
@@ -1,6 +1,7 @@
 window.onload = function(){
         $("#center-logo-container-small").effect( "shake" ,{distance:3},200);
         $("#text-body").effect( "shake" ,{distance:3,direction:"right"},200);
+        $("#calendar-body").effect( "shake" ,{distance:3,direction:"right"},200);
         $("#center-logo-container").effect( "shake" ,{distance:3},200);
         $("#special-links").effect( "shake" ,{distance:3,direction:"right"},200);
       };
