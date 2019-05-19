97e98900fb04aa7de856f6ad4943a5db5181b527
Toothbrush <toothbrush.draw@gmail.com>
2019-05-19 12:10:29 -0700

    Jquery vibrations

---
+++ vibrate.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+        $("#center-logo-container-small").effect( "shake" ,{distance:3},200);
+        $("#text-body").effect( "shake" ,{distance:3,direction:"right"},200);
+        $("#center-logo-container").effect( "shake" ,{distance:3},200);
+        $("#special-links").effect( "shake" ,{distance:3,direction:"right"},200);
+      };
